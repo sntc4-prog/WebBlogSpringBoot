9db85d9949a8203aef02618003df602a26388337
sntc4-prog <55177772+sntc4-prog@users.noreply.github.com>
2020-08-02 00:04:28 +0600

    First commit (Start Spring Boot Project)

---
+++ README.md
@@ -0,0 +1,2 @@
+Demo project on Spring Boot (MVC).
+Web Blog
---
+++ src/test/java/ru/sntc4/WebBlog/WebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sntc4.WebBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
