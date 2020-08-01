8efa6017f7ae029d9ac0ddbfdb06a7c0bb12b2d2
sntc4-prog <55177772+sntc4-prog@users.noreply.github.com>
2020-08-02 01:45:42 +0600

    Added home.html, blog-main.html, about.html, header and footer blocks, created BlogController and MainController , updated application.properties

---
+++ src/main/java/ru/sntc4/WebBlog/controllers/BlogController.java
@@ -0,0 +1,13 @@
+package ru.sntc4.WebBlog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(Model model) {
+        return "blog-main";
+    }
+}
